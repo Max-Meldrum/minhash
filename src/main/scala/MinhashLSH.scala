d77841437a7feac4813f85d069c49eddc011dae5
Max Meldrum <max@meldrum.se>
2017-11-10 18:05:31 +0100

    change to seq

--- src/main/scala/MinhashLSH.scala
+++ src/main/scala/MinhashLSH.scala
@@ -1,32 +1,41 @@
-// Max Meldrum
-
 import scala.io.Source
 import scala.util.Random
-
 
 object MinhashLSH extends App {
   type Document = Set[String]
   type HashedShingles = Set[Int]
-  type Signature = Set[Int]
+  type Signature = Seq[Int]
 
-  val shingleSize = 3
+  val shingleSize = 5
   val minHashFunctions = 100
-  val threshold = 0.8
+  val threshold = 0.5
   val maxShingle = 2147483647
   // Next prime num after maxShingle
   val bigPrime: Long = 2147483869L
 
 
-  val files = Seq("data/data.txt", "data/data1.txt", "data/data2.txt")
+  val files = Seq("data/data.txt", "data/data1.txt", "data/data2.txt", "data/data3.txt", "data/data4.txt")
   val documents: Seq[Document] = files.map(file => getShinglesFromFile(file, shingleSize))
-  val hashedDocuments: Seq[(HashedShingles, Int)] = documents.map(doc => hashShingles(doc)).toIndexedSeq.zipWithIndex
-  val coeffs = randomHashCoefficients(minHashFunctions)
+  val hashedDocuments: Seq[(HashedShingles, Int)] = documents.map(doc => hashShingles(doc))
+    .toIndexedSeq
+    .zipWithIndex
 
-  val signatures: Seq[(Int, Set[Int])] = hashedDocuments.map(doc => {
+  val coeffs = randomHashCoefficients(minHashFunctions)
+  val signatures: Seq[(Int, Seq[Int])] = hashedDocuments.map(doc => {
     (doc._2, minHash(doc._1, coeffs))
   })
 
 
+  // This can probably be improved...
+  val size = signatures.size
+  for (i <- 0 to size-1) {
+    val signature = signatures(i)._2
+    for (j <- (i +1) to (size-1)) {
+      if (similarity(signature, signatures(j)._2) >= threshold) {
+        println("Document " + files(i) + " matches with " + files(j))
+      }
+    }
+  }
 
   /** Get K-shingles from a file
     *
@@ -41,9 +50,10 @@ object MinhashLSH extends App {
     }
   }
 
-  /** For each Document, convert k-shingles to integer
+  /** For each Document, convert k-shingles to integers
     *
     * @param doc
+    * @return HashedShingles set of ints
     */
   private def hashShingles(doc: Document): HashedShingles =
     doc.map(shingle => Math.abs(shingle.hashCode)% maxShingle)
@@ -61,14 +71,32 @@ object MinhashLSH extends App {
     return BigDecimal(intersection.toDouble/union.toDouble).setScale(2, BigDecimal.RoundingMode.HALF_UP)
   }
 
+
+  /** Used to check similarity of two minhashed signatures
+    *
+    * @param sigOne
+    * @param sigTwo
+    * @return
+    */
+  private def similarity(sigOne: Signature, sigTwo: Signature): Double = {
+    var count = 0
+    for (i <- 0 to sigOne.size-1) {
+      if (sigOne(i) == sigTwo(i))
+        count += 1
+    }
+    count.toDouble/sigOne.size.toDouble
+  }
+
   /**
     *
     * @param hashedShingles Shingles converted to integers
     */
-  private def minHash(hashedShingles: HashedShingles, coeffs: Seq[(Int, Int)]): Set[Int] = {
+  private def minHash(hashedShingles: HashedShingles, coeffs: Seq[(Int, Int)]): Seq[Int] = {
     coeffs.map(co => {
-      hashedShingles.map(shingle => generateHash(shingle, co._1, co._2)).min
-    }).toSet
+      val max = Int.MaxValue
+      val result =  hashedShingles.map(shingle => generateHash(shingle, co._1, co._2)).min
+      if (result < max) result else max
+    })
   }
 
 
